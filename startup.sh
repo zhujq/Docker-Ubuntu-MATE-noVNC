bfe1e62d09258448c28667e505bbc51ddb78cb6a
zhujq <38308874+zhujq@users.noreply.github.com>
2021-03-05 23:45:44 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,23 +2,16 @@
 
 if [ ! -f $HOME/.vnc/passwd ] ; then
 
-    if  [ -z "$PASSWORD" ] ; then
-        PASSWORD=`pwgen -c -n -1 12`
-        echo -e "PASSWORD = $PASSWORD" > $HOME/password.txt
-    fi
-
+    
+    PASSWORD=ubuntu
     echo "$USER:$PASSWORD" | chpasswd
 
     # Set up vncserver
     su $USER -c "mkdir $HOME/.vnc && echo '$PASSWORD' | vncpasswd -f > $HOME/.vnc/passwd && chmod 600 $HOME/.vnc/passwd && touch $HOME/.Xresources"
     chown -R $USER:$USER $HOME
-
-    if [ ! -z "$SUDO" ]; then
-        case "$SUDO" in
-            [yY]|[yY][eE][sS])
-                adduser $USER sudo
-        esac
-    fi
+    adduser $USER sudo
+       
+    
 
 else
 
@@ -28,16 +21,6 @@ else
         rm -rf /tmp/.X1*
     fi
 
-fi
-
-if [ ! -z "$NGROK" ] ; then
-        case "$NGROK" in
-            [yY]|[yY][eE][sS])
-                su ubuntu -c "$HOME/ngrok/ngrok http 6080 --log $HOME/ngrok/ngrok.log --log-format json" &
-                sleep 5
-                NGROK_URL=`curl -s http://127.0.0.1:4040/status | grep -P "http://.*?ngrok.io" -oh`
-                su ubuntu -c "echo -e 'Ngrok URL = $NGROK_URL/vnc.html' > $HOME/ngrok/Ngrok_URL.txt"
-        esac
 fi
 
 /usr/bin/supervisord -n
